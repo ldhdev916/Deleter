2ed60d7f3d6dbbe71beafe6926ee94ad83240c67
HappyAndJust <dongheon0916@gmail.com>
2021-10-15 11:42:50 +0900

    Nothing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.31"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "me.dongh"
@@ -22,3 +23,22 @@ tasks.test {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+tasks {
+    named<Jar>("jar") {
+        manifest.attributes(mapOf("Main-Class" to "MainKt"))
+        enabled = false
+    }
+
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        archiveFileName.set(jar.get().archiveFileName)
+    }
+}
+
+tasks {
+    build {
+        dependsOn("shadowJar")
+    }
+}
+
